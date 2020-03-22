c43693cf05dfdb39f9b6238aea112a81e6f52c2f
douglas <douglas06mp@gmail.com>
2020-03-22 16:24:05 +0800

    Updates

---
+++ precache-manifest.610228c69aa2c509cd6d4c8691e9730e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaddd21241979d30f4b7b13ddbec23b4",
+    "url": "/react-todo-context/index.html"
+  },
+  {
+    "revision": "abddff99c71df2488bed",
+    "url": "/react-todo-context/static/css/main.159b4b98.chunk.css"
+  },
+  {
+    "revision": "8ab76a6c223a7c4604b1",
+    "url": "/react-todo-context/static/js/2.9fe7b065.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/react-todo-context/static/js/2.9fe7b065.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abddff99c71df2488bed",
+    "url": "/react-todo-context/static/js/main.531ca82b.chunk.js"
+  },
+  {
+    "revision": "ad1d390cc88d5d1e7d9f",
+    "url": "/react-todo-context/static/js/runtime-main.6f28901a.js"
+  }
+]);
